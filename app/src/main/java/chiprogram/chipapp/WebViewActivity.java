a44b129454d744d94618e2f24150d3cc397f9065
bhatia <friendsat8>
2014-10-29 17:39:26 -0400

    webview activity for viewing pdfs and ppt

---
+++ app/src/main/java/chiprogram/chipapp/WebViewActivity.java
@@ -0,0 +1,73 @@
+package chiprogram.chipapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import chiprogram.chipapp.classes.CHIPLoaderSQL;
+import chiprogram.chipapp.classes.CHIPUser;
+import chiprogram.chipapp.classes.CommonFunctions;
+import chiprogram.chipapp.classes.Content;
+
+
+public class WebViewActivity extends Activity {
+
+    private CHIPUser m_user;
+    private String m_contentId;
+    public static final String CONTENT_ID = "chiprogram.chipapp.CONTENT_ID";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        // Show the Up button in the action bar.
+        setupActionBar();
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+
+        m_user = extras.getParcelable(ProfileActivity.ARGUMENT_USER);
+
+        m_contentId = extras.getString(CONTENT_ID);
+        Content content = CHIPLoaderSQL.getInstance().getContent(m_contentId);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        myWebView.loadUrl(CommonFunctions.convertUrlToGoogleDocsURL(content.getLink()));
+    }
+
+    /**
+     * Set up the {@link android.app.ActionBar}.
+     */
+    private void setupActionBar() {
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.web_view, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
